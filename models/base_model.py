b7f5d235b699ac94240fef861e3f703481db4def
allia-wase <a.umutoniwa1@alustudent.com>
2025-05-08 04:50:48 -0700

    create a base_model.py file

---
+++ models/base_model.py
@@ -0,0 +1,35 @@
+import uuid
+from datetime import datetime
+import models
+
+class BaseModel:
+    """Base class for all models"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update updated_at and save to storage"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Return a dictionary representation of the instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
+
